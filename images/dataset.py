4184d90cbbfbb8ed8029ae51ce16d27403d8c5e7
Satoshi Kobayashi <pikanchisato@yahoo.co.jp>
2020-01-23 11:13:15 +0900

    to Google Colab

--- images/dataset.py
+++ images/dataset.py
@@ -6,12 +6,20 @@ from images.collect import get_list_from_txt
 
 
 class Dataset():
-    def __init__(self, input_dir):
-        johnnys_detail = get_list_from_txt("lists/johnnys_list")
-        others_detail = get_list_from_txt("lists/others_list")
+    def __init__(self, input_dir, colab=False):
+        if colab:
+            johnnys_detail = get_list_from_txt("lists/johnnys_list")
+            others_detail = get_list_from_txt("lists/others_list")
+            group_member = pd.read_csv("lists/group_member.csv")
+        else:
+            colab_path = "/content/isJohnnys/images/"
+            johnnys_detail = get_list_from_txt(colab_path + "lists/johnnys_list")
+            others_detail = get_list_from_txt(colab_path + "lists/others_list")
+            group_member = pd.read_csv(colab_path + "lists/group_member.csv")
+
         detail_label_list = johnnys_detail + others_detail
         self.loader = ImageLoader(input_dir=input_dir, detail_label_list=detail_label_list)
-        self.group_member = pd.read_csv("lists/group_member.csv")
+        self.group_member = group_member
 
     def load_data(self):
         johnnys_images, johnnys_labels, johnnys_detail_labels = self.loader.load_johnnys_images()
