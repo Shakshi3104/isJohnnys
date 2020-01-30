7611f269af2afb8e645637bc9fb7150bd4e5e99a
user <watchosastrom3104@gmail.com>
2020-01-30 11:50:28 +0900

    a little change

--- networks/models.py
+++ networks/models.py
@@ -76,8 +76,10 @@ def pretrained_VGG(weight_layer_num=16, side=64, labels=2, frozen_layer_num=None
     if frozen_block_num is not None:
         frozen_layer_num = block_layer_table[frozen_block_num]
 
-    for i, layer in enumerate(vgg_.layers[:frozen_layer_num]):
+    for layer in vgg_.layers[:frozen_layer_num]:
         layer.trainable = False
+        
+    for i, layer in enumerate(model.layers):
         print("layer " + str(i) + " trainable: " + str(layer.trainable))
 
     return model
